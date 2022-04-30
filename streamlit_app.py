c3002a54aff22039e5033925afd01403ce181742
Vishal Deo <54703708+vishal84520@users.noreply.github.com>
2022-04-30 16:51:30 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+Streamlit.text('Omega 3 & Blueberry Oatmeal')
+Streamlit.text('Kale, Spinach & Rocket Smmothie')
+Streamlit.text('Hard-Boiled Free-Ranger Egg')
